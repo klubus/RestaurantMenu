78ae5870b8856bf4e8379c46bb2065f025ab2009
klubus <72337504+klubus@users.noreply.github.com>
2021-02-22 13:22:45 +0100

    product page

---
+++ Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Baza_Danych
+{
+    class Product
+    {
+        public int Id { get; set; }
+
+        public string Type { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+    }
+}
---
+++ ProductsPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Baza_Danych
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ProductsPage.xaml
+    /// </summary>
+    public partial class ProductsPage : Page
+    {
+        public IProductTypeEnums.ProductTypes ProductType{get; set;}
+        public ProductsPage()
+        {
+            InitializeComponent();
+            SetProductTypeLabel();
+        }
+
+        private void SetProductTypeLabel()
+        {
+            switch(ProductType)
+            {
+                case IProductTypeEnums.ProductTypes.Soups:
+                    ProductTypeLbl.Content = "Zupy";
+                    break;
+                case IProductTypeEnums.ProductTypes.MainDishes:
+                    ProductTypeLbl.Content = "Dania Główne";
+                    break;
+                case IProductTypeEnums.ProductTypes.Desserts:
+                    ProductTypeLbl.Content = "Desery";
+                    break;
+                case IProductTypeEnums.ProductTypes.Drinks:
+                    ProductTypeLbl.Content = "Napoje";
+                    break;
+            }
+        }
+    }
+}
